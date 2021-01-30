fe889bc93c18e05cd1bae5c0fa1fb2ab7a68124b
mehmetmaldar <77964995+mehmetmaldar@users.noreply.github.com>
2021-01-30 03:17:57 +0300

    Create ders1.py

---
+++ ders1.py
@@ -0,0 +1,19 @@
+"""print("merhaba dünya")
+input("bir değer giriniz:")
+a=int(input("bir sayı giriniz:"))
+b=input("bir sayı giriniz")
+toplama=a+b
+print(toplama)
+"""
+"""
+print("mehmet","ahmet",sep="-")
+print(*"mehmet",sep="-")
+print("mehmet",end=".")
+print()
+print("mehmet\tnerde")
+print("mehmet\nnerde")
+"""
+
+a=int(input("bir sayı giriniz:"))
+print(f"girdiğiniz sayı = {a}")
+print("girdiğiniz sayı={}".format(a))
