23877e431d5d470372f1c01c5cff9ae55de86ca1
mehmetmaldar <77964995+mehmetmaldar@users.noreply.github.com>
2021-01-30 03:19:51 +0300

    Create ders4.py

---
+++ ders4.py
@@ -0,0 +1,16 @@
+# diziler ,eleman ekleme ve numaralandırma,eleman atma
+liste =  [43,35,78,13,5,86,90]
+print(len(liste))
+print(liste[3])
+print(liste)
+liste.pop()
+print(liste)
+liste.pop(2)
+print(liste)
+liste+=[7,14]
+print(liste)
+liste.append(17)
+print(liste)
+print(liste[5:2:-1])
+liste.sort()
+print(liste)
