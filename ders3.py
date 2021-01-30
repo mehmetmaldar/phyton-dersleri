5f9faceaa7c7280977ae341f0bffa8b044c17034
mehmetmaldar <77964995+mehmetmaldar@users.noreply.github.com>
2021-01-30 03:19:31 +0300

    Create ders3.py

---
+++ ders3.py
@@ -0,0 +1,16 @@
+#for döngüsü
+"""
+for i in range (1,20):
+    print(i)
+"""
+"""
+for i in range(1,6):
+    a=input(f"{i}.isim giriniz:")
+"""
+#while döngüsü(iken)
+""" 
+i=int(input("bir sayı giriniz:"))
+while i<10:
+    input("bir isim giriniz:")
+    i+=1
+"""
