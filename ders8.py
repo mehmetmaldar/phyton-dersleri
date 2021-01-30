b42314c138fc6ac87b8aaee10c47e511db94081c
mehmetmaldar <77964995+mehmetmaldar@users.noreply.github.com>
2021-01-30 03:21:36 +0300

    Create ders8.py

---
+++ ders8.py
@@ -0,0 +1,41 @@
+# fonksiyonlar
+"""
+def toplama(s1,s2):
+    toplam=s1+s2
+    return toplam;
+print(toplama(100,4))
+"""
+"""
+def isim():
+    print("mehmet")
+
+print("isminiz nedir:")
+isim()
+"""
+"""
+def büyük(s1,s2):
+    if s1>s2:
+        print(s1)
+    elif s1==s2:
+        print("eşit sayıalr")
+    else:
+        print(s2)
+büyük(12,12)
+büyük(14,17)
+büyük(20,18)
+"""
+"""
+def üst(s,üs):
+    üstlü=1
+    for i in range(1,üs+1):
+        üstlü=s*üstlü
+
+    return üstlü;
+
+print(üst(2,3))
+
+"""
+
+
+
+
